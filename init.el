233d05c375f90a54e2d4dd27c4c68b02f32f4a2b
gmambro <gmambro@fb.com>
2021-01-25 11:18:57 -0800

    fix custom-file

--- init.el
+++ init.el
@@ -6,10 +6,10 @@
 
 (push "~/.emacs.d/libs" load-path)
 ;; Load custom file
-(setq custom-file "~/.emacs.d/localconfig.el")
+(setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
-
+(load "~/.emacs.d/localconfig.el")
 
 
 ;; Enable/disable commands
@@ -165,22 +165,3 @@
   (server-start))
 
 ;;; init.el ends here
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(swiper spacemacs-theme ivy auto-package-update cargo markdown-mode flycheck-inline flycheck-inline-mode use-package flycheck-rust rust-mode))
- '(show-paren-mode t)
- 
- )
-
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
