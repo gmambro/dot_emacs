9975680e6de672b69eabe9b4612e75e335f14bba
gmambro <gmambro@fb.com>
2021-01-26 03:26:51 -0800

    random tweaks

--- init.el
+++ init.el
@@ -4,13 +4,29 @@
 ;;; Code:
 
 
-(push "~/.emacs.d/libs" load-path)
-;; Load custom file
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
+; https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
+(defvar modi/gc-cons-threshold--orig gc-cons-threshold)
+(setq gc-cons-threshold (* 100 1024 1024)) ;100 MB before garbage collection
 
+(push "~/.emacs.d/libs" load-path)
+
+;; Set custom file
+(setq custom-file "~/.emacs.d/custom.el")
+
+;; (add-to-list 'load-path "~/.emacs.d/lisp/")
+;; (mapc 'load (file-expand-wildcards "~/.emacs.d/init/*.el"))
+
+;; override local stuff
 (load "~/.emacs.d/localconfig.el")
 
+;; revert buffers automatically when underlying files are changed externally
+(global-auto-revert-mode t)
+
+;; UTF8 world
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
 
 ;; Enable/disable commands
 ;; TODO Review
@@ -18,7 +34,6 @@
 ;;(put 'upcase-region 'disabled t)
 ;;(put 'narrow-to-region 'disabled nil)
 ;;(put 'eval-expression 'disabled nil)
-
 
 ;; Set up the visible bell instead of a beep
 (setq visible-bell t)
@@ -30,6 +45,18 @@
 (setq scroll-preserve-screen-position 1)
 
 (setq show-paren-mode t)
+
+;; view line numbers
+(require 'linum)
+(global-linum-mode 1)
+(setq linum-format "%3d ")
+
+;; view simpler column numbers
+(column-number-mode t)
+
+;; switch multiple buffer with typeahead using C-x b
+(require 'ido)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
 
 (push "/Users/gmambro/.cargo/bin" exec-path)
 
@@ -65,9 +92,8 @@
 
 (use-package ivy
   :diminish
-  :bind (("C-s" . swiper)
+  :bind (
          :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)
          ("C-l" . ivy-alt-done)
          ("C-j" . ivy-next-line)
          ("C-k" . ivy-previous-line)
@@ -75,11 +101,18 @@
          ("C-k" . ivy-previous-line)
          ("C-l" . ivy-done)
          ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
+         )
   :config
-  (ivy-mode 1))
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "(%d/%d) ")
+  )
+
+(use-package swiper
+  :ensure t
+  :config
+  (global-set-key "\C-s" 'swiper))
+
 
 (use-package flycheck
   :hook (prog-mode . flycheck-mode)
@@ -102,14 +135,28 @@
 
 (use-package recentf :ensure t)
 
-(use-package rust-mode :ensure t)
-(use-package
-  flycheck-rust
-  :hook ('flycheck-mode-hook . #'flycheck-rust-setup)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+(use-package recentf
+  :config
+  (setq
+        recentf-max-saved-items 500
+        recentf-max-menu-items 15
+        ;; disable recentf-cleanup on Emacs start, because it can cause
+        ;; problems with remote files
+        recentf-auto-cleanup 'never)
+  (recentf-mode +1))
+
+(use-package toml-mode :ensure t)
+
+(use-package rust-mode :ensure t
+  :hook (rust-mode . lsp)
   :config
   (setq rust-format-on-save t)
   )
-(use-package toml-mode :ensure t)
+(use-package
+  flycheck-rust
+  :hook ('flycheck-mode-hook . #'flycheck-rust-setup)
+  )
 (use-package cargo
   :hook (rust-mode . cargo-minor-mode))
 
@@ -119,13 +166,12 @@
   :commands lsp
   :config
   (setq lsp-enable-file-watchers nil)
-  (setq lsp-rust-rls-server-command (quote ("/home/gmambro/.cargo/bin/rls")))
+  (setq lsp-rust-rls-server-command (quote ("~/.cargo/bin/rustup  run stable rls")))
   (setq lsp-rust-clear-env-rust-log nil)
   )
 
 (use-package lsp-ui :ensure t :commands lsp-ui-mode)
 (use-package company-lsp :ensure t :commands company)
-
 
 (use-package markdown-mode
   :ensure t
@@ -137,31 +183,19 @@
   :init (setq markdown-command "multimarkdown"))
 
 
-;; view line numbers
-(require 'linum)
-(global-linum-mode 1)
-(setq linum-format "%3d ")
+;; VCS stuff
+(use-package magit)
+(use-package monky)
+(use-package treemacs-magit)
 
-;; view simpler column numbers
-(column-number-mode t)
+(ignore-errors
+  (server-start))
 
-;; switch multiple buffer with typeahead using C-x b
-(require 'ido)
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-
-(recentf-mode 1)
-(setq recentf-max-menu-items 25)
-(setq recentf-max-saved-items 25)
-(global-set-key "\C-x\ \C-r" 'recentf-open-files)
-
-
-(load-theme 'spacemacs-dark t)
+(ignore-errors
+  (load-theme 'spacemacs-dark t))
 ;;(setq spacemacs-theme-org-agenda-height nil)
 ;;(setq spacemacs-theme-org-height nil)
 ;; My previous theme (load-theme 'wombat)
 
-
-(ignore-errors
-  (server-start))
 
 ;;; init.el ends here
